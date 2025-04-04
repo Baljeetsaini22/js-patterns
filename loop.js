88cbc247d409bb9ae52e3c02cf5f8d1e86adcfd9
Baljeetsaini22 <baljeetsaini7440@gmail.com>
2025-04-04 17:03:43 +0530

    new pattren

--- loop.js
+++ loop.js
@@ -155,6 +155,8 @@
 // }
 // console.log(z);
 
+
+
 //! => find maximum and minimum number in array 
 
 // let num = [87, 15, 26, 45, 54, 64, 55, 35, 23, 96];
@@ -165,65 +167,27 @@
 // const min = Math.min(...num);
 // console.log(min);
 
-//! => find the number is divisible by 4 or not 
-
-// let a = 5;
-
-// if (a / 4) {
-//     console.log("true")
-// } else {
-//     console.log("false");
-    
-// }
 
 //! => find the Prime number  
 
-// function myFunction(a){
-//     for(i=2; i<=a; i++){
-//      if(a % i != 0){
-//      console.log(" Prime ")
-//      }
-//       else{
-//         a++  // creates infinite loop
-//         if(a % i == 0){
-//         console.log("not prime")
-//         } 
-//         else{
-//          console.log("prime")
-//         }   
-//       }
-//    }
-//     console.log(a)
-//    }
-//    myFunction();
-
-
-
-//! Print pattern of star in landscape with next line
-// let num = 7;
-// let star = "";
-// for (i =0 ; i < num; i++) {
-//     for (i = 0; i < num; i++) {
-//         for (j= 0; j < 5; j++) {
-//             if (j == 0 || j == 4 && i != 0 && i != 3 && i != 6 || i == 0 && j != 4 || i == 3 && j != 4 || i == 6 && j != 4) {
-//                 star += "*\n";
-//             } 
-           
-//         } star += "\t ";
-           
-// } console.log(star);
+// let num=0
+// for(i = 2; i <= 100; i++) {    
+//     for( j = 1; j <= i ; j++) {        
+//         if(i % j == 0)            
+//         num++  
+// }
+// if(num==2)
+// console.log(i)
+// num=0  
 // }
 
-// let num = 5;
-// let star = "";
-// if (num > 1) {
-//     for (let i = 1; i <=num; i++) {    
-//         for (let j = 1; j <= (num); j++) {      
-//             if (i==1 ||  j==1) {     
-//                 star += "* ";    
-//             } 
-//         }
-//         star += "\n"; 
-//     }
-//     console.log(star);
+//! => find the number is divisible by 4 or not 
+
+// let num = prompt("Enter your Number")
+
+// if (num % 4 ===0) {
+//     console.log(num + " is divisible by 4")
+// } else {
+//     console.log(num + " is not divisible by 4");
+    
 // }
